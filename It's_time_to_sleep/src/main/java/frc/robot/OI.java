fab949dbdfdb5c27518ea8b4510434a89661f1ef
JustinDew <Justinluu67@yahoo.com>
2019-03-04 10:15:09 -0800

    Add files via upload

---
+++ It's_time_to_sleep/src/main/java/frc/robot/OI.java
@@ -0,0 +1,69 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import frc.robot.commands.intake;
+import frc.robot.commands.lowerElevator;
+import frc.robot.commands.outtake;
+import frc.robot.commands.raiseElevator;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+  Joystick fightStick;
+  JoystickButton upButton;
+  JoystickButton downButton;
+  JoystickButton outtakeButton;
+  JoystickButton intakeButton;
+
+  public OI() {
+
+    fightStick = new Joystick(0);
+    upButton = new JoystickButton(fightStick, 3);
+    downButton = new JoystickButton(fightStick, 2);
+    intakeButton = new JoystickButton(fightStick, 1);
+
+    upButton.whileHeld(new raiseElevator());
+    downButton.whileHeld(new lowerElevator());
+    intakeButton.whileActive(new intake());
+    outtakeButton.whileHeld(new outtake());
+
+    
+    }
+  //// CREATING BUTTONS
+  // One type of button is a joystick button which is any button on a
+  //// joystick.
+  // You create one by telling it which joystick it's on and which button
+  // number it is.
+  // Joystick stick = new Joystick(port);
+  // Button button = new JoystickButton(stick, buttonNumber);
+
+  // There are a few additional built in buttons you can use. Additionally,
+  // by subclassing Button you can create custom triggers and bind those to
+  // commands the same as any other Button.
+
+  //// TRIGGERING COMMANDS WITH BUTTONS
+  // Once you have a button, it's trivial to bind it to a button in one of
+  // three ways:
+
+  // Start the command when the button is pressed and let it run the command
+  // until it is finished as determined by it's isFinished method.
+  // button.whenPressed(new ExampleCommand());
+
+  // Run the command while the button is being held down and interrupt it once
+  // the button is released.
+  // button.whileHeld(new ExampleCommand());
+
+  // Start the command when the button is released and let it run the command
+  // until it is finished as determined by it's isFinished method.
+  // button.whenReleased(new ExampleCommand());
+}
---
+++ It's_time_to_sleep/src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,42 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.*;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.*;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+
+/**
+ * Add your docs here.
+ */
+public class Elevator extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  TalonSRX elevatorMotor;
+
+  public Elevator(){
+    elevatorMotor = new TalonSRX(RobotMap.ELEVATOR_MOTOR);
+
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+    setDefaultCommand(null);
+  }
+  public void setElevatorSpeed(double speed){
+    elevatorMotor.set(ControlMode.PercentOutput, speed);
+  }
+  public void stopElevator(){
+    elevatorMotor.set(ControlMode.PercentOutput, 0.0);
+  }
+}
